f23e64495235690da8b8f880bfa1d94b2d5a93de
Albert Sabaté <albert@sabatemartinez.com>
2018-03-09 08:55:21 +0800

    Fix errors npm start

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const server = require('http').createServer(app);
-const io = require('../..')(server);
+const io = require('socket.io')(server);
 const port = process.env.PORT || 3000;
 
 // @TODO Add mongoose
